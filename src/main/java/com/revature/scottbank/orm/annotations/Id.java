57a0f2af589f8d5000aa940d839d77e98a0205f9
Tommy Nguyen <minhtuan@vt.edu>
2021-11-29 12:44:59 -0500

    hard coded orm logic into our web app. need to remove when working properly

---
+++ src/main/java/com/revature/scottbank/orm/annotations/Id.java
@@ -0,0 +1,9 @@
+package com.revature.scottbank.orm.annotations;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Id {
+}
